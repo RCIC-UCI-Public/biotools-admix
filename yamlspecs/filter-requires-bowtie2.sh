db4076b941fb7ae542952890c129f59acfc8ada1
nadyawilliams <nadya.williams@gmail.com>
2020-02-04 15:26:35 -0800

    rm extraneous edits

--- yamlspecs/filter-requires-bowtie2.sh
+++ yamlspecs/filter-requires-bowtie2.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 #
 # remove requirement that bowtie2 depends on perl(Math::Random) 
-/usr/lib/rpm/find-requires $* | sed -e '/perl(Math::Random)/d' | sed -e '/^[[:space:]]*$/d' | sed -e '/^#/d'
+/usr/lib/rpm/find-requires $* | sed -e '/perl(Math::Random)/d' 
+
