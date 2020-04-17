f4b23c6a9f40c2683f36c3cc267d727414c357a6
Philip Papadopoulos <ppapadop@uci.edu>
2020-04-17 13:46:03 -0700

    updates for bowtie2 to filter perl requires/provides. Unicycler build for intel-tbb

--- yamlspecs/filter-requires-bowtie2.sh
+++ yamlspecs/filter-requires-bowtie2.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 #
 # remove requirement that bowtie2 depends on perl(Math::Random) 
-/usr/lib/rpm/find-requires $* | sed -e '/perl(Math::Random)/d' 
+# Substitute all references of perl( with perl_{{version}}
+PERLVERSION=$(cat $(dirname $(realpath $0))/PERLVERSION)
+/usr/lib/rpm/find-requires $* | sed -e '/perl(Math::Random)/d'  -e "s/perl(/perl_$PERLVERSION(/g"
 
