d213b4e2db7d63f76f4087138e1f426db35f910e
Nadya Williams <nadya.williams@gmail.com>
2021-04-23 18:20:02 -0700

    use short  and long names for easier module handling add missing filter files

---
+++ yamlspecs/filter-requires-trinity.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# remove requirement that trinity depends on perl packages and extra strings
+# Substitute all references of perl with perl_{{version}}
+
+PERLVERSION=$(cat $(dirname $(realpath $0))/PERLVERSION)
+/usr/lib/rpm/find-requires $* | sed \
+    -e '/perl(XML::Compile/d' \
+    -e '/package/d' \
+    -e '/\/usr\/local\/bin\/perl/d' \
+    -e "s/perl(/perl_$PERLVERSION(/g"
